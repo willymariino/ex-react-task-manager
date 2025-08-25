a99083ede2bf1406c7e17a23f8f4d8c93acab0c1
willymariino <wg.mariino@gmail.com>
2025-08-25 14:44:52 +0200

    milestone 1 completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router-dom"
 
 function Navbar() {
     return (
+
         <nav>
-            <li> <NavLink to="/AddTasks" className="nav-link" >Home Page</NavLink> </li>
-            <li> <NavLink to="/TaskList" className="nav-link">About Us</NavLink> </li>
+            <li> <NavLink to="/AddTasks" className="nav-link" >Lista task</NavLink> </li>
+            <li> <NavLink to="/TaskList" className="nav-link">Aggiungi Task</NavLink> </li>
         </nav>
 
 
