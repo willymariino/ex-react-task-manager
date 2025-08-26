d33603c4769b39fbbc0822bc5af17758378e7d96
willymariino <wg.mariino@gmail.com>
2025-08-26 14:35:43 +0200

    add: dynamic coloring to list items

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -2,11 +2,27 @@ import { memo } from "react"
 
 function TaskRow({ task }) {
 
+    const statusColor = status => {
+        if (status === "To do") {
+            return "To-do"
+        }
+        else if (status === "Doing") {
+            return "Doing"
+        }
+        else if (status === "Done") {
+            return "Done"
+        }
+        else {
+            return "";
+        }
+
+    }
+
     return (
         <>
             <tr>
                 <td>{task.title}</td>
-                <td>{task.status}</td>
+                <td className={statusColor(task.status)}>{task.status}</td>
                 <td>{task.createdAt}</td>
             </tr>
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     return (
 
         <nav>
-            <li> <NavLink to="/AddTasks" className="nav-link" >Lista task</NavLink> </li>
-            <li> <NavLink to="/TaskList" className="nav-link">Aggiungi Task</NavLink> </li>
+            <li> <NavLink to="/AddTasks" className="nav-link" >Aggiungi Task</NavLink> </li>
+            <li> <NavLink to="/TaskList" className="nav-link">Lista Task</NavLink> </li>
         </nav>
 
 
