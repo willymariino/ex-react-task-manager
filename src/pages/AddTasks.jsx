1f5254cc63b563bad97be75c3e0e0ef306519355
willymariino <wg.mariino@gmail.com>
2025-08-29 01:30:10 +0200

    add: implement initial state and handleChange for AddTasks component

--- src/pages/AddTasks.jsx
+++ src/pages/AddTasks.jsx
@@ -1,4 +1,30 @@
+import { useState, useRef } from "react"
+import useTask from "../hooks/useTask"
+
+
 function AddTasks() {
+
+    const { tasks, AddTasks } = useTask()
+
+    const [data, setData] = useState({
+        title: "",
+    })
+
+    const description = useRef()
+    const status = useRef()
+
+    const handleChange = e => {
+
+        let { name, value } = e.target
+
+        setData(prev => ({
+
+            ...prev,
+            [name]: value
+
+        }))
+    }
+
 
     return (
         <>
